eeea25666df4db87907a7d6359d21d9274192b12
Yusfi Adilaksa <yusfi.adilaksa@pricebook.co.id>
2022-03-20 01:04:35 +0700

    create alert for success catch and success release

---
+++ components/SuccessCatchAlert/index.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+
+type Props = {
+  open: boolean;
+  handleClose: () => void;
+};
+
+const SuccessCatchAlert = (props: Props) => {
+  return (
+    <Snackbar
+      autoHideDuration={2000}
+      anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      open={props.open}
+      onClose={props.handleClose}
+    >
+      <Alert severity="success">You successfully catch the pokemon!</Alert>
+    </Snackbar>
+  );
+};
+
+export default SuccessCatchAlert;
---
+++ components/SuccessReleaseAlert/index.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+
+type Props = {
+  open: boolean;
+  handleClose: (e: any, r: any) => void;
+};
+
+const SuccessReleaseAlert = (props: Props) => {
+  return (
+    <Snackbar
+      autoHideDuration={2000}
+      anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      open={props.open}
+      onClose={props.handleClose}
+    >
+      <Alert severity="success">
+        You successfully release the pokemon, go catch others!
+      </Alert>
+    </Snackbar>
+  );
+};
+
+export default SuccessReleaseAlert;
