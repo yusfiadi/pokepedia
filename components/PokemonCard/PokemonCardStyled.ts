e64d04e616e11d38dbad79ec43bb960ed307b463
Yusfi Adilaksa <yusfi.adilaksa@pricebook.co.id>
2022-03-20 01:08:09 +0700

    adjust style for pokemon card component

--- components/PokemonCard/PokemonCardStyled.ts
+++ components/PokemonCard/PokemonCardStyled.ts
@@ -1,15 +1,46 @@
 import styled from "@emotion/styled";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import LoadingButton from "@mui/lab/LoadingButton";
 import CardActions from "@mui/material/CardActions";
 
 export const PokemonName = styled(Typography)`
   line-height: 1;
+  font-size: 16px;
+
+  @media (min-width: 600px) {
+    font-size: 20px;
+  }
 `;
 
 export const CardActionsStyled = styled(CardActions)`
   padding: 16px;
 `;
 
+export const ButtonActions = styled(Button)`
+  font-size: 12px;
+  width: 100%;
+
+  @media (min-width: 600px) {
+    font-size: 14px;
+    width: auto;
+  }
+`;
+
+export const ButtonLoading = styled(LoadingButton)`
+  font-size: 12px;
+  width: 100%;
+
+  @media (min-width: 600px) {
+    font-size: 14px;
+    width: auto;
+  }
+`;
+
 export const TotalOwned = styled.span`
-  margin-left: 16px;
+  font-size: 12px;
+  margin-left: 10px;
+  @media (min-width: 600px) {
+    font-size: 14px;
+  }
 `;
