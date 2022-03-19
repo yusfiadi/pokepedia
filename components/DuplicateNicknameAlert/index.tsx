f9be1d47eb97133a8838a940a9696e78e47f9256
Yusfi Adilaksa <yusfi.adilaksa@pricebook.co.id>
2022-03-20 01:07:26 +0700

    create alert for duplicate nickname

---
+++ components/DuplicateNicknameAlert/index.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+
+type Props = {
+  open: boolean;
+  handleClose: () => void;
+};
+
+const DuplicateNicknameAlert = (props: Props) => {
+  return (
+    <Snackbar
+      autoHideDuration={2000}
+      anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      open={props.open}
+      onClose={props.handleClose}
+    >
+      <Alert severity="info">
+        You already have pokemon that has same nickname with your input, please
+        try again with another nickname!
+      </Alert>
+    </Snackbar>
+  );
+};
+
+export default DuplicateNicknameAlert;
