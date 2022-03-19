3a46c835d54ba7c31f8a45973b5e8072c94eb074
Yusfi Adilaksa <yusfi.adilaksa@pricebook.co.id>
2022-03-20 01:08:51 +0700

    add conditional rendering based on card type either pokemon list or my pokemon card type

--- components/PokemonCard/index.tsx
+++ components/PokemonCard/index.tsx
@@ -2,19 +2,22 @@ import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
 import Link from "next/link";
-import Typography from "@mui/material/Typography";
 
 import * as Styled from "./PokemonCardStyled";
 
 type PokemonCardProps = {
+  cardType: "pokemon-list" | "my-pokemon";
   pokemon: {
+    id: number;
     name: string;
-    url: string;
-    dreamworld: string;
+    url?: string;
+    artwork?: string;
+    nickname?: string;
   };
+  count?: () => void;
+  handleRelease?: () => void;
 };
 
 const PokemonCard = (props: PokemonCardProps) => {
@@ -23,21 +26,40 @@ const PokemonCard = (props: PokemonCardProps) => {
       <CardMedia
         component="img"
         alt={props.pokemon.name}
-        image={props.pokemon.dreamworld}
+        image={
+          props.pokemon.artwork ??
+          `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${props.pokemon.id}.png`
+        }
       />
       <CardContent>
         <Styled.PokemonName variant="h5">
           {props.pokemon.name}
+          <Styled.TotalOwned>
+            {props.cardType === "pokemon-list"
+              ? `(${props.count})`
+              : `(${props.pokemon.nickname})`}
+          </Styled.TotalOwned>
         </Styled.PokemonName>
       </CardContent>
       <Divider />
       <Styled.CardActionsStyled>
-        <Link href={`/details/${props.pokemon.name}`}>
-          <Button variant="outlined" size="small">
-            See Details
-          </Button>
-        </Link>
-        <Styled.TotalOwned>1 owned</Styled.TotalOwned>
+        {props.cardType === "pokemon-list" && (
+          <Link href={`/details/${props.pokemon.name}`}>
+            <Styled.ButtonActions variant="outlined" size="small">
+              See Details
+            </Styled.ButtonActions>
+          </Link>
+        )}
+        {props.cardType === "my-pokemon" && (
+          <Styled.ButtonActions
+            variant="text"
+            color="error"
+            size="small"
+            onClick={props.handleRelease}
+          >
+            Release
+          </Styled.ButtonActions>
+        )}
       </Styled.CardActionsStyled>
     </Card>
   );
